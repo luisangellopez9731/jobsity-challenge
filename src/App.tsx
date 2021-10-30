917213fb5981812eb29d9fbc7b880acb3427a601
luisangellopez9731 <luisangellopezhernandez1997@hotmail.com>
2021-10-30 11:10:25 -0400

    adding banner component, moving background to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import { Header } from "sections";
+import { Banner, Header } from "sections";
+import backgroundSrc from "assets/images/header/background.png";
+
 function App() {
-  return <Header />;
+  return (
+    <>
+      <img src={backgroundSrc} width="100%" style={{ position: "absolute" }} />
+      <div style={{ position: "relative" }}>
+        <Header />
+        <Banner />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -1,14 +1,9 @@
 import { Navbar } from "./navbar";
-import backgroundSrc from "assets/images/header/background.png";
+
 export const Header = () => {
   return (
     <>
-      <img
-        src={backgroundSrc}
-        width="100%"
-        style={{ position: "absolute", zIndex: 1 }}
-      />
-      <div style={{ position: "relative", zIndex: 2 }}>
+      <div>
         <Navbar />
       </div>
     </>
